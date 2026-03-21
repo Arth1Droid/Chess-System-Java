429d3b7ae0375493930b07b85c2606ee20750723
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-21 18:49:31 -0300

    Add Rook possible movements

--- src/chess/pieces/Rook.java
+++ src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -8,6 +9,54 @@ public class Rook extends ChessPiece{
 
 	public Rook(Board board, Color color) {
 		super(board, color);
+	}
+	
+	@Override
+	public boolean[][] possibleMoves() {
+		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		Position p = new Position(0,0);
+		
+		// Above piece
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Left piece
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Right piece
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Below piece
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		return matriz;
 	}
 
 	@Override
