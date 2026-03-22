376ff134af127e9da797288613e84ec7866d380d
ArthDroid1 <barbosarthur7@gmail.com>
2026-03-22 11:14:59 -0300

    Create Queen piece in game

--- src/chess/pieces/Queen.java
+++ src/chess/pieces/Queen.java
@@ -5,15 +5,56 @@ import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
-public class Bishop extends ChessPiece {
+public class Queen extends ChessPiece{
 
-	public Bishop(Board board, Color color) {
+	public Queen(Board board, Color color) {
 		super(board, color);
 	}
-
+	
+	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] matriz = new boolean[getBoard().getRows()][getBoard().getColumns()];
 		Position p = new Position(0,0);
+		
+		// Above piece
+		p.setValues(position.getRow() - 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Left piece
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Right piece
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setColumn(p.getColumn() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
+		
+		// Below piece
+		p.setValues(position.getRow() + 1, position.getColumn());
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+			p.setRow(p.getRow() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p)) {
+			matriz[p.getRow()][p.getColumn()] = true;
+		}
 		
 		// NW piece
 		p.setValues(position.getRow() - 1, position.getColumn() - 1);
@@ -60,7 +101,9 @@ public class Bishop extends ChessPiece {
 
 	@Override
 	public String toString() {
-		return "B";
+		return "Q";
 	}
+	
+	
 
 }
